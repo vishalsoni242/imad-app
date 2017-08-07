29fe675d856c05f4cd49205e2dab57840dc1820d
vishalsoni242 <30750452+vishalsoni242@users.noreply.github.com>
2017-08-07 19:43:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,6 @@
 console.log('Loaded!');
+var element = document.getElementById('main-text');
+element.innerHTML = 'Hello';
 var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginLeft = '100px';
