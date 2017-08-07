84970093df0b43db46f82c73eb0de4ea5ff8847a
vishalsoni242 <30750452+vishalsoni242@users.noreply.github.com>
2017-08-07 09:29:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res) {
-   res.send('Article one request'); 
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
