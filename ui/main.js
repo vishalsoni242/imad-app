ec5a7731ebdbab2b57e855b6b8b782f58f267fb6
vishalsoni242 <30750452+vishalsoni242@users.noreply.github.com>
2017-08-08 20:21:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text'
-    );
-element.innerHTML = 'Hello';
-var img = document.getElementById('madi');
-img.onclick = function() {
-    img.style.marginLeft = '100px';
-};
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function()
+{
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
