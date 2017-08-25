a795d9b0ca632b7a365bcdfb03ac6cffdf07e7e0
vishalsoni242 <30750452+vishalsoni242@users.noreply.github.com>
2017-08-25 21:15:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+var config = {
+    user: 'vishalhari24',
+    database: 'vishalhari24',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: 'db-vishalhari24-16682'
+};
+var bodyParser = require('body-parser');
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -33,6 +41,21 @@ function hash(input,salt) {
 app.get('/hash/:input' , function(req,res) {
    var hashedstring = hash(req.params.input , 'this-is-random-string');
    res.send(hashedstring);
+});
+var pool = new Pool(config);
+app.post('/create-user',function(req,res) {
+    var username = req.body.username;
+    var password = req.body.password;
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password,salt);
+   pool.query ('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result) {
+      if(err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created  '+ username);
+         
+      }
+   });
 });
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
